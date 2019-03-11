2095dabff3ce522e1a3af8367c293823533a49e0
Alex Emelianov <alex.emelianov.kiev@gmail.com>
2019-03-11 22:51:43 +0200

    initial

--- app/code/Protein/OfflineShipping/Model/Carrier/Tablerate.php
+++ app/code/Protein/OfflineShipping/Model/Carrier/Tablerate.php
@@ -3,12 +3,13 @@
 namespace Protein\OfflineShipping\Model\Carrier;
 
 use Magento\OfflineShipping\Model\Carrier\Tablerate as NativeTablerate;
-
+use Magento\Quote\Model\Quote\Address\RateRequest;
 /**
  * Class Tablerate
  * @package Prorein\OfflineShipping\Model\Carrier
  */
-class Tablerate extends NativeTablerate
+class Tablerate extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
+    \Magento\Shipping\Model\Carrier\CarrierInterface
 {
     /**
      * @var string
@@ -26,16 +27,52 @@ class Tablerate extends NativeTablerate
         \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
         \Psr\Log\LoggerInterface $logger,
         \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
-        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $resultMethodFactory,
-        \Protein\OfflineShipping\Model\ResourceModel\Carrier\TablerateFactory $tablerateFactory,
+        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
         array $data = []
     ) {
         $this->_rateResultFactory = $rateResultFactory;
-        $this->_resultMethodFactory = $resultMethodFactory;
-        $this->_tablerateFactory = $tablerateFactory;
-        foreach ($this->getCode('condition_name') as $k => $v) {
-            $this->_conditionNames[] = $k;
-        }
+        $this->_rateMethodFactory = $rateMethodFactory;
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
     }
 
+    /**
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return ['proteintablerate' => $this->getConfigData('name')];
+    }
+
+    /**
+     * @param RateRequest $request
+     * @return bool|Result
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        /** @var \Magento\Shipping\Model\Rate\Result $result */
+        $result = $this->_rateResultFactory->create();
+
+        /** @var \Magento\Quote\Model\Quote\Address\RateResult\Method $method */
+        $method = $this->_rateMethodFactory->create();
+
+        $method->setCarrier('proteintablerate');
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod('proteintablerate');
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        /*you can fetch shipping price from different sources over some APIs, we used price from config.xml - xml node price*/
+        $amount = $this->getConfigData('price');
+
+        $method->setPrice($amount);
+        $method->setCost($amount);
+
+        $result->append($method);
+
+        return $result;
+    }
 }
